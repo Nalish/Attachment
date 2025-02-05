5d2dcff5dd72a3c097f43a5ac37012b321391010
Nalish <sharonnaliaka@gmail.com>
2025-02-05 15:07:07 +0300

    JS variables

---
+++ JAVASCRIPT/variables/variables.js
@@ -0,0 +1,70 @@
+let age=25;
+const schoolName="Greenwood High"
+let studentList=[];
+
+//let is used for variables that can be reassigned later on
+//const is used to declare values that cannot be reassigned and remain the same throughout the code.
+//var is an old way of declaring variables
+
+//2.Which of the following variable names is invalid?
+//let $price = 100;
+//let 1stPlace = "John";- Answer
+// let _score = 89;
+// let userName = "Alice";
+
+
+//5. Why is the following variable name incorrect?
+// const #taxRate = 0.16;
+//Answer- It begins with a #
+
+// 6. Rewrite this variable name to follow best practices:
+// let MyvariableNAME = "JavaScript;
+//Answer- myVariableName
+
+// 3. What will be the output of the following?
+// console.log(typeof "Hello");-string
+// console.log(typeof 99);-number
+// console.log(typeof true);-Boolean
+// console.log(typeof undefined);-null
+
+
+
+// 8.
+// Identify the data types in this array:
+// let data = ["Kenya", 34, false, { country: "USA" }, null];
+// "kenya"-String, 34-Number, false-Boolean {country:USA}-Object
+
+
+//9.BigInt refers to an integer that has exceeded 2 power 53-1
+ //it is defined by adding an n to the value
+
+ let schools=40000000000n
+
+
+
+
+// 11. Create an object person with properties name, age, and city.
+let Person={
+    name:"David",
+    age:23,
+    city:"Nairobi"}
+// 12. Add a new property email to the person object.
+Person.email="sharon@gmail.com"
+// 13. Declare an array fruits with three fruit names.
+let fruits=["mangaoes","oranges","pineapples"]
+// 14. Access the second item in the fruits array.
+fruits[1]
+// 15. What will be the output of the following?
+// console.log("5" + 2);-52
+// console.log("5" - 2); 52
+
+// 16. Convert the string "100" into a number.
+let num= parseInt(100)
+
+// 17. Convert the number 50 into a string.
+let num2= toString(50)
+// 18. What will be the result of this operation?
+// console.log(5 + true);-6
+
+
+
